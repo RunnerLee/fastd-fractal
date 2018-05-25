8476aa23d6956d57b8a493c2c5a18a130c01703d
RunnerLee <runnerleer@gmail.com>
2018-05-25 14:44:11 +0800

    add abstract transformer

---
+++ src/AbstractTransformer.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @author: RunnerLee
+ * @email: runnerleer@gmail.com
+ * @time: 2018-05
+ */
+
+namespace Runner\FastdFractal;
+
+use League\Fractal\TransformerAbstract;
+
+class AbstractTransformer extends TransformerAbstract
+{
+    /**
+     * @param mixed $data
+     * @param null  $transformer
+     * @param null  $resourceKey
+     *
+     * @return \League\Fractal\Resource\Primitive
+     */
+    public function primitive($data, $transformer = null, $resourceKey = null)
+    {
+        return parent::primitive(
+            $data,
+            is_null($transformer) ? null : fractal()->transformer($transformer),
+            $resourceKey
+        );
+    }
+
+    /**
+     * @param mixed                        $data
+     * @param callable|TransformerAbstract $transformer
+     * @param null                         $resourceKey
+     *
+     * @return \League\Fractal\Resource\Item
+     */
+    public function item($data, $transformer, $resourceKey = null)
+    {
+        return parent::item($data, fractal()->transformer($transformer), $resourceKey);
+    }
+
+    /**
+     * @param mixed                        $data
+     * @param callable|TransformerAbstract $transformer
+     * @param null                         $resourceKey
+     *
+     * @return \League\Fractal\Resource\Collection
+     */
+    public function collection($data, $transformer, $resourceKey = null)
+    {
+        return parent::collection($data, fractal()->transformer($transformer), $resourceKey);
+    }
+}
